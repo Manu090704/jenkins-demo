9d874662d39bacc4ffc7d5e50eb3db63952281dd
Manu090704 <A01711581@tec.mx>
2026-02-26 23:11:20 -0600

    menos un test

--- sum.test.js
+++ sum.test.js
@@ -5,10 +5,6 @@ test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
 
-test('adds -1 + -1 to equal -2', () => {
-    expect(sum(-1, -1)).toBe(-2);
-});
-
 test('rest 5 - 2 to equal 3', () => {
     expect(rest(5, 2)).toBe(3);
 });
