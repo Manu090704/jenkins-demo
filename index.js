b70300aef655665c8d6bed3c26636fc99f6decea
Manu090704 <A01711581@tec.mx>
2026-02-26 22:47:02 -0600

    pruebas de jenkins

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ const sum = (a, b) => {
     return a + b;
 }
 
+const rest = (a, b) => {
+    return a - b;
+}
+
 
 module.exports = sum;
+module.exports = rest;
--- sum.test.js
+++ sum.test.js
@@ -1,4 +1,5 @@
 const sum = require('./index');
+const rest = require('./index');
 
 test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3);
@@ -6,4 +7,12 @@ test('adds 1 + 2 to equal 3', () => {
 
 test('adds -1 + -1 to equal -2', () => {
     expect(sum(-1, -1)).toBe(-2);
+});
+
+test('rest 5 - 2 to equal 3', () => {
+    expect(rest(5, 2)).toBe(3);
+});
+
+test('rest -1 - -1 to equal 0', () => {
+    expect(rest(-1, -1)).toBe(0);
 });
